63d7705b1d7c32e72e581361b92dfcc20f90a12f
Vladislav <vladislav.ulyanenko@gmail.com>
2019-05-19 18:48:00 +0300

    docs folder

---
+++ docs/precache-manifest.08e750c00f4120d065f86d5710484446.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6291c899be1ac62a99da908cb1a978ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a636656d381ad5c4d011",
+    "url": "/static/css/main.10648199.chunk.css"
+  },
+  {
+    "revision": "eca33caa0db88f2e23bf",
+    "url": "/static/js/2.b9445595.chunk.js"
+  },
+  {
+    "revision": "a636656d381ad5c4d011",
+    "url": "/static/js/main.29ca2a74.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
